aec922ad93584f8c285ede12294faadfafcad7e2
waarrk <48860242+waarrk@users.noreply.github.com>
2022-07-14 13:32:43 +0900

    shisokuenzan

--- helloworld.R
+++ helloworld.R
@@ -1,1 +1,27 @@
 print("Hello World!")
+
+3 + 5
+10 - 3
+2 * 3
+100 / 20
+(12 + 34 - 56) * 78 / 90
+
+x <- 2
+y <- 3 + 4
+z <- x + y
+z
+
+floor(1.999)
+x <- sqrt(4)
+x
+y <- sqrt(x)
+y
+
+x <- c(2, 3, 5, 7, 11)
+sqrt(x)
+mean(x)
+summary(x)
+
+x <- c(2, 4, 9, 5, 3)
+sort(x)
+sort(x, decreasing = TRUE)
