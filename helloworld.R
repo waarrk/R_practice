dbbf0c4317dda1d56b9e05156dca0c9ddd024372
waarrk <48860242+waarrk@users.noreply.github.com>
2022-07-14 14:47:50 +0900

    norm

--- helloworld.R
+++ helloworld.R
@@ -20,8 +20,65 @@ y
 x <- c(2, 3, 5, 7, 11)
 sqrt(x)
 mean(x)
-summary(x)
+summary(x)s
 
 x <- c(2, 4, 9, 5, 3)
 sort(x)
 sort(x, decreasing = TRUE)
+
+objects()
+#rm(z)
+#rm(list = ls(all=TRUE))
+
+getwd()
+
+exp(1 + 2i)
+log(2 + 1i) - log(5 + 1i)
+log(-2)
+log(-2 + 0i)
+
+1:4
+x <- c(1, 2)
+y <- c(3, 4)
+x %o% y
+
+gamma(5+1)
+
+x <- 1
+x
+
+print(x)
+print("one")
+print("one", quote=F)
+page(x)
+options(digits = 10)
+sqrt(2)
+
+sprintf("%s if %f feet tall\n", "Sven", 7)
+sprintf("%s if %i feet tall\n", "Sven", 7)
+
+print(pi)
+sprintf("%d", as.integer(pi))
+
+cat("nyaaan")
+
+x <- data.frame(matrix(1:9,3,3))
+x
+str(x)
+summary(x)
+x <- data.frame(matrix(1:8,2,4))
+comment(x) <- c("x: 2 x 4 matrix", "may 5, 2004")
+str(x)
+comment(x)
+
+x <- c()
+x
+
+date()
+
+x <- c(1.0, 2.0, 3.0, 4.0, 5.0)
+length(x)
+x <- rnorm(1000)
+x
+hist(x)
+
